f1a8698614cff5cc3e0aec8801d91dfa595e71e2
Stephen Foo Bar <stephenfoobar@gmail.com>
2020-08-11 14:57:13 -0400

    Test 5: Validate a missing config setup. - Fail.

--- feed/list_test.go
+++ feed/list_test.go
@@ -2,10 +2,14 @@ package feed
 
 import (
 	"testing"
+
+	"github.com/StephenFooBar/gopher-pouches/config"
 	"github.com/stretchr/testify/assert"
 )
 
-func DoNothing()
-{
+func TestListFeedShouldShowErrorMessageWhenDataStoreIsNotSet(t *testing.T) {
+	expected := "Data store is not set. Please run config to set data store information."
+	actual := List(config.Config{""})
+	assert.Equal(t, expected, actual.message)
+	assert.Equal(t, false, actual.success)
 }
-
