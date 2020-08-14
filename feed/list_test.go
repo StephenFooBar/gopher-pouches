62c7c06183aa5a33b3839c930da1a4db17591bc9
Stephen Foo Bar <stephenfoobar@gmail.com>
2020-08-14 14:50:09 -0400

    Refactor 5: Adding a missing refactor.

--- feed/list_test.go
+++ feed/list_test.go
@@ -3,12 +3,13 @@ package feed
 import (
 	"testing"
 
+	"github.com/StephenFooBar/gopher-pouches/command"
 	"github.com/StephenFooBar/gopher-pouches/config"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestListFeedShouldShowErrorMessageWhenDataStoreIsNotSet(t *testing.T) {
-	expected := "Data store is not set. Please run config to set data store information."
+	expected := command.DataStoreNotSet
 	actual := List(config.Config{""})
 	assert.Equal(t, expected, actual.Message)
 	assert.Equal(t, false, actual.Success)
