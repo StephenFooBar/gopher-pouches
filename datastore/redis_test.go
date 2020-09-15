dd639441ba9ff05dea342400f1dcf8afa9b58fa3
Stephen Foo Bar <stephenfoobar@gmail.com>
2020-09-15 16:27:02 -0400

    Test 12: Error check when Data connection string is empty connecting to Redis store.

---
+++ datastore/redis_test.go
@@ -0,0 +1,14 @@
+package datastore
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetFeedsReturnsErrorWhenConnectionIsEmpty(t *testing.T) {
+	redis := Redis{""}
+	actual, err := redis.GetFeeds("")
+	assert.Nil(t, actual)
+	assert.NotNil(t, err)
+}
