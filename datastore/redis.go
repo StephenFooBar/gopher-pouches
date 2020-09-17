180f7f493ac47816492aefe948f8aa628f6317f4
Stephen Foo Bar <stephenfoobar@gmail.com>
2020-09-17 14:19:57 -0400

    Refactor 13: Parse out host and database with splits when there are equal signs in connection string.

--- datastore/redis.go
+++ datastore/redis.go
@@ -15,19 +15,31 @@ type Redis struct {
 
 func GetInstance(connection string) Redis {
 	//separate by comma "redis0:6380,redis1:6380,allowAdmin=true"
-	//host:6379,database=0
+	//host=:6379,database=0
 	conn := strings.Split(connection, ",")
+	host := getHost(conn)
+	db := getDb(conn)
+	return Redis{connection, host, db}
+}
+
+func getDb(conn []string) string {
 	if len(conn) == 1 {
-		return Redis{connection, conn[0], ""}
+		return ""
 	}
-	return Redis{connection, conn[0], conn[1]}
+	db := strings.Split(conn[1], "=")
+	return db[len(db)-1]
+}
+
+func getHost(conn []string) string {
+	host := strings.Split(conn[0], "=")
+	return host[len(host)-1]
 }
 
 func (r Redis) GetFeeds() ([]Feed, error) {
 	if r.Connection == "" {
 		return nil, errors.New(EmptyConnection)
 	}
-	conn, err := redis.Dial("tcp", r.Connection)
+	conn, err := redis.Dial("tcp", r.Host)
 	if err != nil {
 		return nil, err
 	}
